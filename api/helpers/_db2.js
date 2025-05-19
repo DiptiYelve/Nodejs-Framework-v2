94b6cfd7a730b0eb406fe63c90a54f7fcf04bfd4
Dipti Yelve <dipti.yelve@smartinfologiks.com>
2025-05-19 16:52:13 +0530

    refs : #1126 use async/await in _db.js file instead of callback

--- api/helpers/_db2.js
+++ api/helpers/_db2.js
@@ -34,227 +34,214 @@ module.exports = function (server, restify) {
 						return;
 					}
 
-					console.log("MYSQL Initialized with _db2 - MYSQL0");
+					console.log("MYSQL Initialized with _db3 - MYSQL0");
 				});
 			}
 		}
 	}
 
 	//Standard MySQL
-	db2_query = function (dbkey, sql, params, callback) {
-		if (_MYSQL[dbkey] == null) {
-			console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-			return callback(false);
-		}
-		if (CONFIG.log_sql) {
-			console.log("SQL", sql, params);
-		}
-		//server.mysql.query(sql, params, function(err, results, fields) {
-		_MYSQL[dbkey].query(sql, params, function (err, results, fields) {
-			// console.log(err,results,fields);
-			if (err) {
-				callback(false);
-				return;
-			}
-			if (results.length <= 0) {
-				return callback([]);
-			}
-			results = JSON.parse(JSON.stringify(results));
-			callback(results);
-		});
-	}
+	db3_query = function (dbkey, sql, params, connection) {
+		return new Promise((resolve, reject) => {
+			if (connection) {
+				if (CONFIG.log_sql) {
+					console.log("SQL", sql, params);
+				}
 
-	db2_selectQ = function (dbkey, table, columns, where, whereParams, callback, additionalQueryParams) {
-		if (_MYSQL[dbkey] == null) {
-			console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-			return callback(false);
-		}
-
-		if (Array.isArray(columns)) columnsStr = columns.join(",");
-		else columnsStr = columns;
-
-		var sql = "SELECT " + columnsStr + " FROM " + table + " ";
-
-		if (where != null) {
-			var sqlWhere = [];
-			if (typeof where == "object" && !Array.isArray(where)) {
-				_.each(where, function (a, b) {
-					if (a == "RAW") {
-						sqlWhere.push(b);
-					} else if (Array.isArray(a) && a.length == 2) {
-						sqlWhere.push(b + a[1] + "'" + a[0] + "'");
-					} else {
-						sqlWhere.push(b + "='" + a + "'");
+				connection.query(sql, params, function (err, results, fields) {
+					if (err) {
+						return reject(err);
 					}
+					if (results.length <= 0) {
+						return resolve([]);
+					}
+					resolve(JSON.parse(JSON.stringify(results)));
 				});
 			} else {
-				sqlWhere.push(where);
+				if (_MYSQL[dbkey] == null) {
+					return reject(`Database Not Connected for ${dbkey}`);
+				}
+
+				if (CONFIG.log_sql) {
+					console.log("SQL", sql, params);
+				}
+
+				_MYSQL[dbkey].query(sql, params, function (err, results, fields) {
+					if (err) {
+						return reject(err);
+					}
+					if (results.length <= 0) {
+						return resolve([]);
+					}
+					resolve(JSON.parse(JSON.stringify(results)));
+
+				});
 			}
-
-			if (sqlWhere.length > 0) {
-				sql += " WHERE " + sqlWhere.join(" AND ");
-			}
-		}
-
-		if (additionalQueryParams != null && additionalQueryParams.length > 0) {
-			sql += additionalQueryParams;
-		}
-
-		// console.log("_selectQ", sql);
-		if (CONFIG.log_sql) {
-			console.log("SQL", sql, whereParams);
-		}
-		//server.mysql.query(sql, whereParams, function(err, results, fields) {
-		_MYSQL[dbkey].query(sql, whereParams, function (err, results, fields) {
-			if (err || results.length <= 0) {
-				if (err) console.log(err);
-				callback(false);
-				return;
-			}
-
-			results = JSON.parse(JSON.stringify(results));
-			callback(results);
 		});
-	}
-
-	db2_insertQ1 = function (dbkey, table, data, connection, callback) {
-		if (connection) {
-			cols = [];
-			quest = [];
-			vals = [];
-			_.each(data, function (a, b) {
-				cols.push(b);
-				vals.push(a);
-				quest.push("?");
-			});
-
-			var sql = "INSERT INTO " + table + " (" + cols.join(",") + ") VALUES (" + quest.join(",") + ")";
-
-			if (CONFIG.log_sql) {
-				console.log("SQL", sql, vals);
-			}
-
-			connection.query(sql, vals, function (err, results, fields) {
-				if (err) {
-					return callback(false, err.code, err.sqlMessage);
-				}
-
-				callback(results.insertId);
-			});
-		} else {
-			if (_MYSQL[dbkey] == null) {
-				console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-				return callback(false);
-			}
-
-			cols = [];
-			quest = [];
-			vals = [];
-			_.each(data, function (a, b) {
-				cols.push(b);
-				vals.push(a);
-				quest.push("?");
-			});
-
-			var sql = "INSERT INTO " + table + " (" + cols.join(",") + ") VALUES (" + quest.join(",") + ")";
-
-			if (CONFIG.log_sql) {
-				console.log("SQL", sql, vals);
-			}
-
-			_MYSQL[dbkey].query(sql, vals, function (err, results, fields) {
-				if (err) {
-					return callback(false, err.code, err.sqlMessage);
-				}
-
-				callback(results.insertId);
-			});
-		}
 	};
 
-	db2_insert_batchQ = function (dbkey, table, data, callback) {
-		if (_MYSQL[dbkey] == null) {
-			console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-			return callback(false);
-		}
+	db3_selectQ = function (dbkey, table, columns, where, whereParams, connection, additionalQueryParams) {
+		return new Promise((resolve, reject) => {
+			let columnsStr;
 
-		if (data[0] == null) {
-			return callback(false, "Data Not Defined");
-		}
+			if (Array.isArray(columns)) columnsStr = columns.join(",");
+			else columnsStr = columns;
 
-		let cols = Object.keys(data[0]);
-		let values = data.map(obj => cols.map(key => obj[key]));
+			let sql = "SELECT " + columnsStr + " FROM " + table + " ";
 
-
-		var sql = "INSERT INTO " + table + " (" + cols.join(",") + ") VALUES ?";
-
-		if (CONFIG.log_sql) {
-			console.log("SQL", sql, data);
-		}
-
-		//server.mysql.query(sql, [values], function(err, results, fields) {
-		_MYSQL[dbkey].query(sql, [values], function (err, results, fields) {
-			if (err) {
-				if (err) console.log(err);
-				return callback(false);
-			}
-			callback(true);
-		});
-	}
-
-	db2_deleteQ = function (dbkey, table, where, callback) {
-		if (_MYSQL[dbkey] == null) {
-			console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-			return callback(false);
-		}
-
-		sqlWhere = [];
-		if (typeof where == "object" && !Array.isArray(where)) {
-			_.each(where, function (a, b) {
-				if (a == "RAW") {
-					sqlWhere.push(b);
-				} else if (Array.isArray(a) && a.length == 2) {
-					sqlWhere.push(b + a[1] + "'" + a[0] + "'");
+			if (where != null) {
+				let sqlWhere = [];
+				if (typeof where == "object" && !Array.isArray(where)) {
+					_.each(where, function (a, b) {
+						if (a == "RAW") {
+							sqlWhere.push(b);
+						} else if (Array.isArray(a) && a.length == 2) {
+							sqlWhere.push(b + a[1] + "'" + a[0] + "'");
+						} else {
+							sqlWhere.push(b + "='" + a + "'");
+						}
+					});
 				} else {
-					sqlWhere.push(b + "='" + a + "'");
+					sqlWhere.push(where);
 				}
-			});
-		} else {
-			sqlWhere.push(where);
-		}
 
-		var sql = "DELETE FROM " + table + " WHERE " + sqlWhere.join(" AND ");
-
-		if (CONFIG.log_sql) {
-			console.log("SQL", sql, sqlWhere);
-		}
-
-		//server.mysql.query(sql, vals, function(err, results, fields) {
-		_MYSQL[dbkey].query(sql, function (err, results, fields) {
-			if (err) {
-				return callback(false);
+				if (sqlWhere.length > 0) {
+					sql += " WHERE " + sqlWhere.join(" AND ");
+				}
 			}
-			callback(true);
+
+			if (additionalQueryParams != null && additionalQueryParams.length > 0) {
+				sql += additionalQueryParams;
+			}
+
+			if (CONFIG.log_sql) {
+				console.log("SQL", sql, whereParams);
+			}
+
+			const queryCallback = (err, results) => {
+				if (err || results.length <= 0) {
+					console.log(err || "No results found");
+					reject(err || "No results found");
+					return;
+				}
+
+				results = JSON.parse(JSON.stringify(results));
+				resolve(results);
+			};
+
+			if (connection) {
+				connection.query(sql, whereParams, queryCallback);
+			} else {
+				if (_MYSQL[dbkey] == null) {
+					console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
+					reject("Database not connected");
+					return;
+				}
+
+				_MYSQL[dbkey].query(sql, whereParams, queryCallback);
+			}
 		});
-	}
+	};
 
-	db2_updateQ = function (dbkey, table, data, where, connection, callback) {
-		if (connection) {
-			var fData = [];
-			var vals = [];
-
+	db3_insertQ1 = function (dbkey, table, data, connection) {
+		return new Promise((resolve, reject) => {
+			let cols = [], quest = [], vals = [];
 			_.each(data, function (a, b) {
-				fData.push(b + "=?");
+				cols.push(b);
 				vals.push(a);
+				quest.push("?");
 			});
 
-			var sqlWhere = [];
+			const sql = `INSERT INTO ${table} (${cols.join(",")}) VALUES (${quest.join(",")})`;
 
-			if (typeof where == "object" && !Array.isArray(where)) {
+			if (CONFIG.log_sql) {
+				console.log("SQL", sql, vals);
+			}
+
+			if (connection) {
+				connection.query(sql, vals, function (err, results) {
+					if (err) {
+						return reject(err);
+					}
+					resolve(results.insertId);
+				});
+			} else {
+				if (_MYSQL[dbkey] == null) {
+					return reject(`Database Not Connected for ${dbkey}`);
+				}
+
+				_MYSQL[dbkey].query(sql, vals, function (err, results) {
+					if (err) {
+						return reject(err);
+					}
+					resolve(results.insertId);
+				});
+			}
+		});
+	};
+
+	db3_insert_batchQ = function (dbkey, table, data, connection) {
+		return new Promise((resolve, reject) => {
+			if (connection) {
+				if (data[0] == null) {
+					return reject("Data Not Defined");
+				}
+
+				let cols = Object.keys(data[0]);
+				let values = data.map(obj => cols.map(key => obj[key]));
+
+				var sql = "INSERT INTO " + table + " (" + cols.join(",") + ") VALUES ?";
+
+				if (CONFIG.log_sql) {
+					console.log("SQL", sql, data);
+				}
+
+				connection.query(sql, [values], function (err, results, fields) {
+					if (err) {
+						console.log(err);
+						return reject(err);
+					}
+					resolve(true);
+				});
+
+			} else {
+				if (_MYSQL[dbkey] == null) {
+					console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
+					return reject("Database Not Connected");
+				}
+
+				if (data[0] == null) {
+					return reject("Data Not Defined");
+				}
+
+				let cols = Object.keys(data[0]);
+				let values = data.map(obj => cols.map(key => obj[key]));
+
+				var sql = "INSERT INTO " + table + " (" + cols.join(",") + ") VALUES ?";
+
+				if (CONFIG.log_sql) {
+					console.log("SQL", sql, data);
+				}
+
+				_MYSQL[dbkey].query(sql, [values], function (err, results, fields) {
+					if (err) {
+						console.log(err);
+						return reject(err);
+					}
+					resolve(true);
+				});
+			}
+		});
+	};
+
+	db3_deleteQ = function (dbkey, table, where, connection) {
+		return new Promise((resolve, reject) => {
+			let sqlWhere = [];
+			if (typeof where === "object" && !Array.isArray(where)) {
 				_.each(where, function (a, b) {
-					if (a == "RAW") {
+					if (a === "RAW") {
 						sqlWhere.push(b);
-					} else if (Array.isArray(a) && a.length == 2) {
+					} else if (Array.isArray(a) && a.length === 2) {
 						sqlWhere.push(b + a[1] + "'" + a[0] + "'");
 					} else {
 						sqlWhere.push(b + "='" + a + "'");
@@ -264,37 +251,56 @@ module.exports = function (server, restify) {
 				sqlWhere.push(where);
 			}
 
-			var sql = "UPDATE " + table + " SET " + fData.join(",") + " WHERE " + sqlWhere.join(" AND ");
+			const sql = "DELETE FROM " + table + " WHERE " + sqlWhere.join(" AND ");
 
 			if (CONFIG.log_sql) {
-				console.log("SQL", sql, vals);
+				console.log("SQL", sql, sqlWhere);
 			}
 
-			connection.query(sql, vals, function (err, results, fields) {
+			const queryCallback = (err, results, fields) => {
 				if (err) {
 					console.log(err);
-					return callback(false);
+					reject(false);
+					return;
+				}else if (results.affectedRows === 0) {
+					console.log("Rows affected: ", results.affectedRows);
+					reject(false);
+					return;
 				}
-				callback(true);
-			});
-		} else {
-			if (_MYSQL[dbkey] == null) {
-				console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-				return callback(false);
+				resolve(true);
+			};
+
+			if (connection) {
+				connection.query(sql, queryCallback);
+			} else {
+				if (_MYSQL[dbkey] === null) {
+					console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
+					reject(false);
+					return;
+				}
+
+				_MYSQL[dbkey].query(sql, queryCallback);
 			}
+		});
+	};
+
+	db3_updateQ = function (dbkey, table, data, where, connection) {
+		return new Promise((resolve, reject) => {
 			var fData = [];
 			var vals = [];
+
 			_.each(data, function (a, b) {
 				fData.push(b + "=?");
 				vals.push(a);
 			});
-	
+
 			var sqlWhere = [];
-			if (typeof where == "object" && !Array.isArray(where)) {
+
+			if (typeof where === "object" && !Array.isArray(where)) {
 				_.each(where, function (a, b) {
-					if (a == "RAW") {
+					if (a === "RAW") {
 						sqlWhere.push(b);
-					} else if (Array.isArray(a) && a.length == 2) {
+					} else if (Array.isArray(a) && a.length === 2) {
 						sqlWhere.push(b + a[1] + "'" + a[0] + "'");
 					} else {
 						sqlWhere.push(b + "='" + a + "'");
@@ -303,71 +309,87 @@ module.exports = function (server, restify) {
 			} else {
 				sqlWhere.push(where);
 			}
-	
+
 			var sql = "UPDATE " + table + " SET " + fData.join(",") + " WHERE " + sqlWhere.join(" AND ");
-	
-			//console.log(sql);
+
 			if (CONFIG.log_sql) {
 				console.log("SQL", sql, vals);
 			}
-	
-			//server.mysql.query(sql, vals, function(err, results, fields) {
-			_MYSQL[dbkey].query(sql, vals, function (err, results, fields) {
+
+			const queryCallback = (err, results, fields) => {
 				if (err) {
-					if (err) console.log(err);
-					return callback(false);
+					console.log(err);
+					reject(false);
+					return;
+				}else if (results.affectedRows === 0){
+					console.log("Rows affected: ", results.affectedRows);
+					reject(false);
+					return;
 				}
-				callback(true);
-			});
-		}
-	};
+				
+				resolve(true);
+			};
 
-	db2_startTransaction = function (dbkey, callback) {
-		console.log("db2_startTransaction - ", dbkey)
-		if (_MYSQL[dbkey] == null) {
-			console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
-			return callback(false);
-		}
-
-		_MYSQL[dbkey].getConnection(function (err, connection) {
-			if (err || connection == null) {
-				console.log("\x1b[31m%s\x1b[0m", `Error getting connection for ${dbkey}`);
-				return callback(false);
-			}
-
-			connection.beginTransaction(function (err) {
-				if (err) {
-					console.log("\x1b[31m%s\x1b[0m", `Error starting transaction for ${dbkey}`);
-					connection.release();
-					return callback(false);
-				}
-				console.log(`Transaction started for ${dbkey}`);
-				callback(null, connection);
-			});
-		});
-	};
-
-	db2_commitTransaction = function (connection, callback) {
-		connection.commit(function (err) {
-			if (err) {
-				console.log("\x1b[31m%s\x1b[0m", `Error committing transaction`);
-				connection.rollback(function () {
-					connection.release();
-					callback(false);
-				});
+			if (connection) {
+				connection.query(sql, vals, queryCallback);
 			} else {
-				console.log("Transaction committed successfully");
-				connection.release();
-				callback(true);
+				if (_MYSQL[dbkey] === null) {
+					console.log("\x1b[31m%s\x1b[0m", `DATABASE Not Connected for ${dbkey}`);
+					reject(false);
+					return;
+				}
+
+				_MYSQL[dbkey].query(sql, vals, queryCallback);
 			}
 		});
 	};
 
-	db2_rollbackTransaction = function (connection, callback) {
-		connection.rollback(function () {
-			console.log("\x1b[31m%s\x1b[0m", "Transaction rolled back");
-			connection.release();
-			callback(false);
+	db3_startTransaction = function (dbkey) {
+		return new Promise((resolve, reject) => {
+			if (_MYSQL[dbkey] == null) {
+				return reject(`Database Not Connected for ${dbkey}`);
+			}
+
+			_MYSQL[dbkey].getConnection(function (err, connection) {
+				if (err || connection == null) {
+					return reject(`Error getting connection for ${dbkey}`);
+				}
+
+				connection.beginTransaction(function (err) {
+					if (err) {
+						connection.release();
+						return reject(`Error starting transaction for ${dbkey}`);
+					}
+					resolve(connection);
+				});
+			});
+		});
+	};
+
+	db3_commitTransaction = function (connection) {
+		return new Promise((resolve, reject) => {
+			connection.commit(function (err) {
+				if (err) {
+					connection.rollback(function () {
+						connection.release();
+						reject("Error committing transaction");
+					});
+				} else {
+					console.log("Transaction committed successfully");
+					connection.release();
+					resolve(true);
+				}
+			});
+		});
+	};
+
+	db3_rollbackTransaction = function (connection) {
+		return new Promise((resolve, reject) => {
+			connection.rollback(function () {
+				console.log("Transaction rolled back");
+				connection.release();
+				reject("Transaction rolled back");
+			});
 		});
 	};
 
